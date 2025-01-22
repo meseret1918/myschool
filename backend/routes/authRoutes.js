b7f3f77c9d9cf7ac9f3c7f1708c33c205d841843
meseret1918 <meseret1918@gmail.com>
2025-01-22 20:08:31 +0300

    Implement or update authentication-related API routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController'); // Import login controller
+const { register, login } = require('../controllers/authController'); 
 
 // Register route
 router.post('/register', register);
 
 // Login route
-router.post('/login', login);  // Define the login route
+router.post('/login', login);  
 
 module.exports = router;
